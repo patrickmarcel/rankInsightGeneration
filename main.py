4e9dd876bfee1a522b870d9aadb427c30d26f9f8
Alexandre Chanson <a.chanson.tours@gmail.com>
2024-08-22 14:35:12 +0200

    fixed a bug in rank construction

--- main.py
+++ main.py
@@ -7,11 +7,15 @@ import utilities
 from utilities import powerset, jaccard_similarity
 from plotStuff import plot_curves
 from dbStuff import execute_query, connect_to_db, close_connection
-from statStuff import benjamini_hochberg_gpt, welch_ttest, permutation_test, compute_skewness, compute_kendall_tau, benjamini_hochberg, benjamini_hochberg_statmod
+from statStuff import welch_ttest, permutation_test, compute_skewness, compute_kendall_tau, benjamini_hochberg, \
+    benjamini_hochberg_statmod
 import time
 from rankingFromPairwise import computeRanksForAll, balanced_rank_estimation, merge_sort
 
+from scipy.stats import permutation_test as sp_permutation_test
+
 import pandas as pd
+
 
 def claireStat(skew1, skew2, count1, count2, threshold=0.049):
     """
@@ -28,17 +32,15 @@ def claireStat(skew1, skew2, count1, count2, threshold=0.049):
     - skewness: boolean indicating if Welch test can be used
     """
 
-    stat = abs((skew1/count1) - (skew2/count2))
-    if stat<threshold:
+    stat = abs((skew1 / count1) - (skew2 / count2))
+    if stat < threshold:
         # test can be used
         return True
     else:
         return False
 
 
-
 def emptyGB(conn, nb):
-
     #queryEmptyGb = ("SELECT " + sel + ","
     #                + " rank () over (  order by " + meas + " desc ) as rank" +
     #                " FROM " + table + " group by " + sel + " limit " + str(sizeOfVals) + ";")
@@ -46,8 +48,8 @@ def emptyGB(conn, nb):
     #                + " rank () over (  order by " + meas + " desc ) as rank" +
     #                " FROM " + table + " group by " + sel + ";")
     #queryEmptyGb = ("SELECT " + sel + ","
-     #               + " rank () over (  order by " + meas + " desc ) as rank" +
-      #              " FROM " + table +  " WHERE " + sel + " in (" + hyp + ") group by " + sel +  ";")
+    #               + " rank () over (  order by " + meas + " desc ) as rank" +
+    #              " FROM " + table +  " WHERE " + sel + " in (" + hyp + ") group by " + sel +  ";")
 
     #hyp = ""
     #for i in range(len(vals)):
@@ -57,21 +59,20 @@ def emptyGB(conn, nb):
 
     queryEmptyGb = ("SELECT " + sel + ","
                     + " rank () over (  order by " + meas + " desc ) as rank" +
-                    " FROM " + table +  " group by " + sel + " limit " + str(nb) + ";")
+                    " FROM " + table + " group by " + sel + " limit " + str(nb) + ";")
 
     #print(queryEmptyGb)
     resultEmptyGb = execute_query(conn, queryEmptyGb)
 
     queryEmptyGbAll = ("SELECT " + sel + ","
-                    + " rank () over (  order by " + meas + " desc ) as rank" +
-                    " FROM " + table + " group by " + sel + ";")
+                       + " rank () over (  order by " + meas + " desc ) as rank" +
+                       " FROM " + table + " group by " + sel + ";")
     resultEmptyGbAll = execute_query(conn, queryEmptyGbAll)
 
     return resultEmptyGb, resultEmptyGbAll
 
 
-
-def generateRandomQuery(pwsert,valsToSelect,hypothesis):
+def generateRandomQuery(pwsert, valsToSelect, hypothesis):
     nb = random.randint(0, len(pwsert) - 1)
     gb = pwsert[nb]
     strgb = ""
@@ -101,14 +102,13 @@ def generateRandomQuery(pwsert,valsToSelect,hypothesis):
     queryValues = ("SELECT measure FROM (SELECT " + strgb + "," + sel + "," + meas + " as measure FROM "
                    + table + " WHERE " + sel + " in " + str(valsToSelect) + " group by " + strgb + "," + sel + " ) x;")
 
-
     queryExcept = ("select " + strgb + "," + sel + ", rank from  (" + query + " ) t3 except all " + queryHyp + " ")
 
     queryCountGb = ("select count(*) from (" + queryHyp + ") t4;")
     queryCountExcept = ("select count(*) from (" + queryExcept + ") t5;")
 
     #return query, queryHyp, queryValues, queryExcept, strgb
-    return queryValues,queryCountGb,queryCountExcept
+    return queryValues, queryCountGb, queryCountExcept
 
 
 def getValues(queryValues, vals, v, conn):
@@ -122,26 +122,25 @@ def getValues(queryValues, vals, v, conn):
     return data
 
 
-
 def getSample(conn, meas, measBase, table, sel, sampleSize, method):
     # sampling using postgresql: https://www.postgresql.org/docs/current/sql-select.html#SQL-FROM
     # system (faster) is block based, bernouili (slower) is row based
 
     querySample = (
-            "SELECT " + sel + ", " + measBase + " FROM " + table + " TABLESAMPLE " + method + " (" + str(sampleSize) + ");")
+            "SELECT " + sel + ", " + measBase + " FROM " + table + " TABLESAMPLE " + method + " (" + str(
+        sampleSize) + ");")
     # print(querySample)
     resultVals = execute_query(conn, querySample)
     print("Sample size in tuples: ", len(resultVals))
     return resultVals
 
 
-def computeBHcorrection(pairwiseComparison, alpha = 0.05):
-
+def computeBHcorrection(pairwiseComparison, alpha=0.05):
     # rejected, corrected_p_values = benjamini_hochberg_gpt(tabPValues, alpha)
     # print("Rejected hypotheses:", rejected)
     # print("raw p-values:", tabPValues)
     # print("Corrected p-values (gpt):", corrected_p_values)
-    tabPValues=[]
+    tabPValues = []
     for p in pairwiseComparison:
         tabPValues.append(p[4])
 
@@ -150,7 +149,7 @@ def computeBHcorrection(pairwiseComparison, alpha = 0.05):
 
     print("nb of True in rejected: ", utilities.nbTrueInList(rejected))
 
-    pairwiseComp2=[]
+    pairwiseComp2 = []
     i = 0
     nbChanges = 0
     for c in pairwiseComparison:
@@ -160,9 +159,9 @@ def computeBHcorrection(pairwiseComparison, alpha = 0.05):
         if corrected[i] < 0.05 and c[3] > 0:
             comp = 1
         if comp != c[2]:
-            nbChanges=nbChanges+1
+            nbChanges = nbChanges + 1
         pairwiseComp2.append((c[0], c[1], comp, c[2], corrected[i]))
-        i=i+1
+        i = i + 1
 
     print("Number of BH corrections: ", nbChanges)
 
@@ -179,14 +178,12 @@ def generateComparisonsWithMergeSort(Sels, S):
             b = claireStat(S[i - 1][2], S[j][2], S[i - 1][1], S[j][1])
             claireTab.append((S[i - 1][0], S[j][0], b, S[i - 1][3], S[j][3]))
 
-
     #claireComp = [(x[0],x[1],x[2]) for x in claireTab]
     #print("claireComp: ", claireTab)
     print("merge: ", merge_sort(Sels, claireTab))
     print("pairwise: ", rankingFromPairwise.pairwiseComparison)
 
     return rankingFromPairwise.pairwiseComparison
-
 
 
 def generateAllComparisons(Sels, S, nbOfComparisons):
@@ -212,7 +209,7 @@ def generateAllComparisons(Sels, S, nbOfComparisons):
                     comp = -1
                 if p_value < 0.05 and t_stat > 0:
                     comp = 1
-                pairwiseComparison.append((S[i - 1][0], S[j][0], comp,t_stat,float(p_value)))
+                pairwiseComparison.append((S[i - 1][0], S[j][0], comp, t_stat, float(p_value)))
             else:
                 # print("Permutation test is used")
                 observed_t_stat, p_value, permuted_t_stats, conclusion = permutation_test(S[i - 1][3], S[j][3])
@@ -229,20 +226,15 @@ def generateAllComparisons(Sels, S, nbOfComparisons):
                     comp = 1
                 pairwiseComparison.append((S[i - 1][0], S[j][0], comp, observed_t_stat, float(p_value)))
 
-    pairwiseComparison=computeBHcorrection(pairwiseComparison, 0.05)
+    pairwiseComparison = computeBHcorrection(pairwiseComparison, 0.05)
     return pairwiseComparison
 
 
-
 def generateHypothesisTest(conn, meas, measBase, table, sel, sampleSize, method):
-
     resultVals = getSample(conn, meas, measBase, table, sel, sampleSize, method)
 
-    Sels = tuple([x[0] for x in resultVals])
-    Sels = list(dict.fromkeys(Sels))
-    #print(Sels)
-    Vals = tuple([x[1] for x in resultVals])
-    #print(Vals)
+    # get adom values
+    Sels = list(set([x[0] for x in resultVals]))
 
     #analyse sample for each adom value: value, nb of measures, skewness, and tuples
     S = []
@@ -250,7 +242,7 @@ def generateHypothesisTest(conn, meas, measBase, table, sel, sampleSize, method)
 
         data = []
         for row in resultVals:
-            if row[0]==v:
+            if row[0] == v:
                 data.append(float(row[1]))
 
         nvalues = len(data)
@@ -262,43 +254,41 @@ def generateHypothesisTest(conn, meas, measBase, table, sel, sampleSize, method)
 
     # nlog(n) comparisons enough for recovering the true ranking when commarisons are certain (not noisy)
     # we should try less
-    nbOfComparisons=len(Sels)*math.log(len(Sels),2)
+    nbOfComparisons = len(Sels) * math.log(len(Sels), 2)
     #print("Number of comparisons to make: " + str(nbOfComparisons))
 
     #pairwiseComparison=generateAllComparisons(Sels, S, nbOfComparisons)
 
-    pairwiseComparison=generateComparisonsWithMergeSort(Sels, S)
+    pairwiseComparison = generateComparisonsWithMergeSort(Sels, S)
 
     # ranking
     #ranks = balanced_rank_estimation(pairwiseComparison)
     #print("Balanced Rank Estimation:", ranks)
-    ranks=computeRanksForAll(pairwiseComparison,Sels)
+    ranks = computeRanksForAll(pairwiseComparison, Sels)
 
     sorted_items = sorted(ranks.items(), key=lambda item: item[1], reverse=True)
-    #print(sorted_items)
 
-    hypothesis=[]
-    rank=0
+    # Construct a rank from the number of comparison won for each adom values
+    hypothesis = []
+    rank = 0
     for s in sorted_items:
         if rank == 0:
-            rank=1
-            hypothesis.append((s[0],rank))
-            val=s[1]
+            rank = 1
+            hypothesis.append((s[0], rank))
+            val = s[1]
         else:
             if s[1] == val:
                 hypothesis.append((s[0], rank))
+                val = s[1]
             else:
-                rank=rank+1
+                rank = rank + 1
                 hypothesis.append((s[0], rank))
-
-    print("hypothesis", hypothesis)
+                val = s[1]
 
     return hypothesis
 
 
-
-def hoeffdingForRank(groupbyAtt, n, valsToSelect,limitedHyp):
-
+def hoeffdingForRank(groupbyAtt, n, valsToSelect, limitedHyp):
     print("Size of confidence interval around p: " + str(epsilon))
     print("Probability is of making a mistake: " + str(alpha))
 
@@ -315,14 +305,12 @@ def hoeffdingForRank(groupbyAtt, n, valsToSelect,limitedHyp):
 
     #print("Hypothesis is:" + str(hypothesis))
 
-
     nbTests = 0
     for i in range(n):
 
         # generate the random query
         # query, queryHyp, queryValues, queryExcept, strgb = generateRandomQuery(pwsert,hypothesis)
-        queryValues, queryCountGb, queryCountExcept = generateRandomQuery(pwset, valsToSelect,limitedHyp)
-
+        queryValues, queryCountGb, queryCountExcept = generateRandomQuery(pwset, valsToSelect, limitedHyp)
 
         # strategy: use the db engine to check whether the hypothesis holds
         # cross join hypothesis to chosen group by set
@@ -350,41 +338,41 @@ def hoeffdingForRank(groupbyAtt, n, valsToSelect,limitedHyp):
 
         #nbTests = nbTests + 1
 
-    expectedValue=sum(H) / len(H)
+    expectedValue = sum(H) / len(H)
     print("Expected value is: " + str(sum(H) / len(H)))
     return expectedValue
 
 
 #parameters
 
-table="fact_table"
-measures=["nb_flights","departure_delay","late_aircraft"]
+table = "fact_table"
+measures = ["nb_flights", "departure_delay", "late_aircraft"]
 #groupbyAtt=["departure_airport","date","departure_hour","flight"]
 #sel="airline"
 #meas="avg(departure_delay)"
 #measBase="departure_delay"
-groupbyAtt=["departure_airport","date","departure_hour","flight"]
-sel="airline"
-meas="avg(departure_delay)"
-measBase="departure_delay"
+groupbyAtt = ["departure_airport", "date", "departure_hour", "flight"]
+sel = "airline"
+meas = "avg(departure_delay)"
+measBase = "departure_delay"
 
 # number of values of adom to consider - top ones after hypothesis is generated
-nbAdomVals=5
+nbAdomVals = 5
 
 # for Hoeffding
-epsilon=0.01
-alpha=0.01
-p=0
-H=[]
-threshold=0.1 # 10% of tuples violating the order
-n=math.log(2/alpha,10) / pow(2,epsilon*epsilon)
-n=math.ceil(n)
+epsilon = 0.01
+alpha = 0.01
+p = 0
+H = []
+threshold = 0.1  # 10% of tuples violating the order
+n = math.log(2 / alpha, 10) / pow(2, epsilon * epsilon)
+n = math.ceil(n)
 
 # for DB sampling
-sampleSize=30
-samplingMethod='BERNOULLI' # or SYSTEM
+sampleSize = 30
+samplingMethod = 'BERNOULLI'  # or SYSTEM
 
-nbruns=10
+nbruns = 10
 
 # TODO
 #  check stability of hypothesis - BRE was generated by GPT...
@@ -398,9 +386,9 @@ nbruns=10
 if __name__ == "__main__":
 
     # Database connection parameters
-    dbname = "flight_dw"
-    user = ""
-    password = ""
+    dbname = "covid"
+    user = "marcel"
+    password = "1598marcel"
     host = "localhost"
     port = "5432"
 
@@ -409,31 +397,31 @@ if __name__ == "__main__":
 
     if conn:
 
-        tabHypo=[]
-        resultRuns=[]
+        tabHypo = []
+        resultRuns = []
         for i in range(nbruns):
-            rankingFromPairwise.pairwiseComparison=[]
+            rankingFromPairwise.pairwiseComparison = []
 
-            start_time=time.time()
+            start_time = time.time()
             #generate hypothesis: ordering of members such that mean is greater (statistically significant on sample)
-            hypothesis=generateHypothesisTest(conn, meas, measBase, table, sel, sampleSize, samplingMethod)
+            hypothesis = generateHypothesisTest(conn, meas, measBase, table, sel, sampleSize, samplingMethod)
 
             #print("Hypothesis as predicted: ", hypothesis)
 
             # limit hypothesis to top nbAdomVals
-            limitedHyp=[]
-            valsToSelect=[]
-            j=0
+            limitedHyp = []
+            valsToSelect = []
+            j = 0
             for h in hypothesis:
-                if(h[1]<=nbAdomVals and j<nbAdomVals):
+                if (h[1] <= nbAdomVals and j < nbAdomVals):
                     limitedHyp.append(h)
                     valsToSelect.append(h[0])
-                    j=j+1
+                    j = j + 1
             #print("Hypothesis limited: ", limitedHyp)
             #print("vals: ",valsToSelect)
 
             # should only be done once
-            emptyGBresult, emptyGBresultAll=emptyGB(conn,nbAdomVals)
+            emptyGBresult, emptyGBresultAll = emptyGB(conn, nbAdomVals)
             print("Empty GB says:", emptyGBresult)
 
             # compute kendall tau between hypothesis and emptyGB
@@ -466,20 +454,19 @@ if __name__ == "__main__":
             #print(vals)
             #print(tuple(valsToSelect))
 
-            expected=hoeffdingForRank(groupbyAtt, n, tuple(valsToSelect),limitedHyp)
+            expected = hoeffdingForRank(groupbyAtt, n, tuple(valsToSelect), limitedHyp)
 
             end_time = time.time()
             elapsed_time = end_time - start_time
 
-            resultRuns.append((i, float(tau),expected,elapsed_time))
-
+            resultRuns.append((i, float(tau), expected, elapsed_time))
 
         print(resultRuns)
         print(tabHypo)
         # compute hamming dist in tabhypo or jaccard
 
-        names=['tau','expected','time']
-        title='Sample size=' + str(sampleSize)
-        plot_curves(resultRuns,  names, 'time', 'expected', title)
+        names = ['tau', 'expected', 'time']
+        title = 'Sample size=' + str(sampleSize)
+        plot_curves(resultRuns, names, 'time', 'expected', title)
         # Close the connection
         close_connection(conn)
