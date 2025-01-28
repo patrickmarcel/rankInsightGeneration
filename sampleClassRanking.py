4f301ed7951b528396e60faf5860836f9e03e3f9
patrickmarcel <patrick.marcel.gm@gmail.com>
2025-01-28 14:36:04 +0100

    various minor bugfixes and cleanups

--- sampleClassRanking.py
+++ sampleClassRanking.py
@@ -157,7 +157,66 @@ class SampleRanking:
 
     # this should be moved elsewhere
     def runStatisticalTest(self, S, test):
-        # so far does nothing, if needed copy code in Lattice
+        b = statStuff.claireStat(S[0][2], S[1][2], S[0][1], S[1][1])
+        pairwiseComparison = []
+        # claireTab.append((S[i - 1][0], S[j][0], b, S[i - 1][3], S[j][3]))
+        match test:
+            case 'stat':
+                if b:
+                    # print("Welch test can be used")
+                    # self.nbWelch = self.nbWelch + 1
+                    comp = 0  # not significant
+                    if len(S[0][3]) <= 1 or len(S[1][3]) <= 1:
+                        comp = -2
+                    else:
+                        t_stat, p_value, conclusion = statStuff.welch_ttest(S[0][3], S[1][3])
+                        if p_value < 0.05 and t_stat < 0:
+                            comp = -1
+                        if p_value < 0.05 and t_stat > 0:
+                            comp = 1
+                    # pairwiseComparison.append((S[0][0], S[1][0], comp, t_stat, float(p_value)))
+                else:
+                    # print("Permutation test is used")
+                    # self.nbPerm = self.nbPerm + 1
+                    comp = 0  # not significant
+                    if len(S[0][3] <= 1) or len(S[1][3] <= 1):
+                        comp = -2
+                    else:
+                        observed_t_stat, p_value, permuted_t_stats, conclusion = statStuff.permutation_test(S[0][3], S[1][3])
+                        if p_value < 0.05 and observed_t_stat < 0:
+                            comp = -1
+                        if p_value < 0.05 and observed_t_stat > 0:
+                            comp = 1
+                    # pairwiseComparison.append((S[0][0], S[1][0], comp, observed_t_stat, float(p_value)))
+            case 'Welch':
+                # only Welch
+                # self.nbWelch = self.nbWelch + 1
+                comp = 0  # not significant
+                if len(S[0][3]) <= 1 or len(S[1][3]) <= 1:
+                    comp = -2
+                else:
+                    t_stat, p_value, conclusion = statStuff.welch_ttest(S[0][3], S[1][3])
+                    # if len(S[0][3])==0 or len(S[1][3])==0:
+                    #    print("empty sample !!")
+                    if p_value < 0.05 and t_stat < 0:
+                        comp = -1
+                    if p_value < 0.05 and t_stat > 0:
+                        comp = 1
+            #                pairwiseComparison.append((S[0][0], S[1][0], comp, t_stat, float(p_value)))
+            case 'Permutation':
+                # only permutation
+                # self.nbPerm = self.nbPerm + 1
+                comp = 0  # not significant
+                if len(S[0][3]) <= 1 or len(S[1][3]) <= 1:
+                    comp = -2
+                else:
+                    observed_t_stat, p_value, permuted_t_stats, conclusion = statStuff.permutation_test(S[0][3], S[1][3])
+                    if p_value < 0.05 and observed_t_stat < 0:
+                        comp = -1
+                    if p_value < 0.05 and observed_t_stat > 0:
+                        comp = 1
+                # pairwiseComparison.append((S[0][0], S[1][0], comp, observed_t_stat, float(p_value)))
+        return comp
         return
 
     #this should be moved elsewhere
